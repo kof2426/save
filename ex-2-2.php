f576bf10fe21266f20ac9c108f3e6e672881a07e
kof2426 <kof2426@naver.com>
2020-12-17 23:00:34 +0900

    add php file[ex]

---
+++ ex-2-2.php
@@ -0,0 +1,10 @@
+<?php
+	echo "<br> 변수 선언하여 출력하기 <br>";
+	$num = 53;           //정수형 변수
+	$Num = 263.37;          //실수형 변수
+	$s_name = "홍길동";      //문자열형 변수
+	echo "-----------------------<br>";
+	echo "1. \$num = $num <br>";
+	echo "2. \$Num = $Num <br>";
+	echo "3. \$s_name = $s_name <br>";
+?>
